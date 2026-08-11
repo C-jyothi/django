6474a8fc637cf6a351377f30f5e28cc381d92add
Cjyothi <chandrajyothi161@gmail.com>
2026-08-11 10:31:16 +0530

    Implement employee view to display a list of employees with details

--- company/employee/views.py
+++ company/employee/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+def employees(request):
+    employee_list = [
+        {
+            "name": "Anu",
+            "job_title": "Software Developer",
+            "salary": 50000,
+            "full_time": True
+        },
+        {
+            "name": "Rahul",
+            "job_title": "Designer",
+            "salary": 40000,
+            "full_time": False
+        },
+        {
+            "name": "Meera",
+            "job_title": "Project Manager",
+            "salary": 60000,
+            "full_time": True
+        }
+    ]
+
+    return render(request, "employees.html", {
+        "employees": employee_list
+    })
